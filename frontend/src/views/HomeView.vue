de27665c1bd5da667ca6d7ed33b1975ada7cc9c1
Luminozen <luminozen@gmail.com>
2022-07-13 19:54:35 +0300

    Определен тестовый фронт

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,14 +5,29 @@
   </div>
 </template>
 
-<script>
+<script setup>
+/* eslint-disable */
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+import axios from 'axios'
+import {ref} from 'vue'
+
+let data = ref([])
+
+const BASE_URL = axios.create(
+  {
+    baseURL: 'http://127.0.0.1:8000/api/',
+    headers: {
+      contentType: "application/json"
+    }
   }
-}
+)
+
+BASE_URL.get('boards/list/').then(response => {
+  console.log(response);
+  data.value = response.data
+})
+
+
 </script>
