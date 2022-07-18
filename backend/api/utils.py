37927f19a31066a3e50c0277be02e160c0405907
Luminozen <luminozen@gmail.com>
2022-07-18 20:41:48 +0300

    Настройки базы

---
+++ backend/api/utils.py
@@ -0,0 +1,47 @@
+from django.db import connections
+def call_stored_function(function_name, db_name='default', single=False, *args):
+    try:
+        with connections[db_name].cursor() as cursor:
+            result = []
+ 
+            cursor.callproc(function_name, args)
+            desc = cursor.description
+            row_count = cursor.rowcount
+ 
+            if(row_count):
+                result = [
+                    dict(zip([col.name for col in desc], row))
+                    for row in cursor.fetchmany(row_count)
+                ]
+ 
+            if single:
+                if len(result):
+                    return result[0]
+ 
+            return result
+    except Exception as e:
+        raise e
+ 
+ 
+def call_stored_procedure(proc_name, db_name="default", *args):
+    try:
+        with connections[db_name].cursor() as cursor:
+            result = {}
+            i = 0
+            while len(args) > i:
+                result[i] = "%s"
+                i = i + 1
+            proc_param = ",".join(str(key) for key in result.values())
+            cursor.execute("call "+proc_name+"("+proc_param+")", args)
+ 
+            desc = cursor.description
+ 
+            # 
+            if not desc:
+                result = {'p_result': 0, 'p_resultmessage': ''}
+            else:
+                result = dict(zip([col.name for col in desc], cursor.fetchone()))
+ 
+            return result
+    except Exception as e:
+        raise(e)
